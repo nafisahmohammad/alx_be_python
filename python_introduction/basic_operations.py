6b4441e8ae5833926b55339beb4d98b3e8093e1d
nafisahmohammad <nafisahmohammad1616@gmail.com>
2025-11-07 17:06:01 +0100

    basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,17 @@
+# Assign 2 numbers
+
+number1 = 10
+number2 = 5
+
+# perform addition, subtraction and mulitplication
+
+addition = number1 + number2
+subtraction = number1 - number2
+mulitplication = number1 * number2
+
+#print results
+
+print(f'addition of {number1} and {number2} is {addition}')
+print(f'subtraction of {number1} and {number2} is {subtraction}')
+print(f'multiplication of {number1} and {number2} is {mulitplication}')
+
