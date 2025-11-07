69c959df30a087b3d7f3fc7eb2ed34f2093a550f
nafisahmohammad <nafisahmohammad1616@gmail.com>
2025-11-07 22:56:23 +0100

    python_introduction

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,10 @@
+#future age calculator
+
+current_age = int(input("how old are you?"))
+
+#calculate the agein 2050(27 years after 2023)
+
+future_age_2050 = current_age + 27
+
+#print the result
+print("in 20250,you will be" + str(future_age_2050) + "years old.")
