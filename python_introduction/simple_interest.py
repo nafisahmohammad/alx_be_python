899ba5cfe8df70b1269aa27490adc09661e69283
nafisahmohammad <nafisahmohammad1616@gmail.com>
2025-11-07 19:25:10 +0100

    python_instructions

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,12 @@
+#variables
+
+principal = 1000
+rate = 0.05
+time = 3
+
+# calculate simple interest
+interest = principal * rate * time
+
+#print the result
+
+print(f'the simple interest is: {interest}')
