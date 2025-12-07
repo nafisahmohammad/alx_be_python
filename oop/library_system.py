19c301f74c3d2c17b65ef2b5554f65fdf33db05d
nafisahmohammad <nafisahmohammad1616@gmail.com>
2025-12-07 23:31:09 +0100

    OOP

--- oop/library_system.py
+++ oop/library_system.py
@@ -24,3 +24,16 @@ class PrintBook(Book):
     def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def list_books(self):
+        for book in self.books:
+            print(book)
+
+
